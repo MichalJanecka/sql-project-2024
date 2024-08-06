33504250b8886520652cd39fa02e5a8428463a10
MichalJanecka <167630007+MichalJanecka@users.noreply.github.com>
2024-08-06 15:17:00 +0200

    Removing the table creation

--- Research question 5.sql
+++ Research question 5.sql
@@ -1,12 +1,3 @@
--- Table 2: creation
-CREATE OR REPLACE TABLE t_michal_janecka_project_SQL_secondary_final AS
-	SELECT *
-	FROM v_michal_yoy_price_growth vmypg 
-	JOIN v_michal_yoy_wage_growth vmywg 
-	ON vmypg.selected_year = vmywg.payroll_year
-	JOIN economies
-	ON vmywg.payroll_year = economies.`year`;
-
 -- Correlation of GDP growth, average gross wages and food prices
 SELECT 
 	country,
