12b81ce72a7bc2437cf352a33353203bdfba281a
MichalJanecka <167630007+MichalJanecka@users.noreply.github.com>
2024-08-13 15:32:57 +0200

    Load time optimalization

--- t_michal_janecka_project_SQL_primary_final.sql
+++ t_michal_janecka_project_SQL_primary_final.sql
@@ -1,7 +1,17 @@
 -- Table 1: creation
-CREATE OR REPLACE TABLE t_michal_janecka_project_SQL_primary_final AS
-SELECT 
-	cpc.name AS food_category,
+CREATE TEMPORARY TABLE temp_czechia_price AS
+SELECT *
+FROM czechia_price
+WHERE region_code IS NULL;
+
+CREATE TEMPORARY TABLE temp_czechia_payroll AS
+SELECT *
+FROM czechia_payroll
+WHERE value_type_code = '5958';
+
+CREATE OR REPLACE TABLE t_Michal_Janecka_project_SQL_primary_final AS
+SELECT
+    cpc.name AS food_category,
 	cpc.code AS food_code,
 	cpr.value AS price, 
 	cpr.date_from, 
@@ -9,14 +19,14 @@ SELECT
 	cpay.value AS average_wage, 
 	cpay.payroll_year,
 	cpib.name AS industry
-FROM czechia_price AS cpr
-JOIN czechia_payroll AS cpay 
-	ON YEAR(cpr.date_from) = cpay.payroll_year
-	AND cpay.value_type_code = 5958
-JOIN czechia_price_category cpc 
-	ON cpr.category_code = cpc.code 
-JOIN czechia_payroll_industry_branch cpib 
-	ON cpay.industry_branch_code = cpib.code;
-	
+FROM temp_czechia_price AS cpr
+JOIN temp_czechia_payroll AS cpay 
+    ON YEAR(cpr.date_from) = cpay.payroll_year
+JOIN czechia_price_category AS cpc 
+    ON cpr.category_code = cpc.code
+JOIN czechia_payroll_industry_branch AS cpib 
+    ON cpay.industry_branch_code = cpib.code;
+
+   
 
 	
