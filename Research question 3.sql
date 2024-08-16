e907d12b356abd1ef6ab82a92bf2ff8e926ebf0e
MichalJanecka <167630007+MichalJanecka@users.noreply.github.com>
2024-08-16 10:11:47 +0200

    Edit of primary solution

--- Research question 3.sql
+++ Research question 3.sql
@@ -17,7 +17,23 @@ FROM t_michal_janecka_project_SQL_primary_final
 GROUP BY food_code, YEAR(date_from)
 ORDER BY food_code, selected_year;
 
--- Year-on-year comparison
+-- Primary solution: Overall year-on-year comparison (2006-2018)
+WITH YearlyChanges AS (
+    SELECT 
+        food_code,
+        selected_year,
+        avg_price,
+        (avg_price - LAG(avg_price) OVER (PARTITION BY food_code ORDER BY selected_year)) / LAG(avg_price) OVER (PARTITION BY food_code ORDER BY selected_year) * 100 AS YOY_Perc_Difference
+    FROM v_michal_yoy_growth
+)
+SELECT 
+    food_code,
+    ROUND(AVG(YOY_Perc_Difference), 2) AS Avg_YOY_Perc_Difference
+FROM YearlyChanges
+GROUP BY food_code
+ORDER BY Avg_YOY_Perc_Difference;
+
+-- Secondary (alternative) solution: Year-on-year comparison of prices within the individual years
 SELECT 
 	food_code,
 	selected_year,
@@ -26,6 +42,3 @@ SELECT
 	ROUND((avg_price - LAG(avg_price, 1) OVER (PARTITION BY food_code ORDER BY food_code, selected_year)) / LAG(avg_price, 1) OVER (PARTITION BY food_code ORDER BY food_code, selected_year), 4) * 100 AS YOY_Perc_Difference
 FROM v_michal_yoy_growth
 ORDER BY YOY_Perc_Difference;
-
-
-
