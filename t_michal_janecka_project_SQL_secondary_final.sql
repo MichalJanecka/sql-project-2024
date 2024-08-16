80836e773350c091cec623b4fdd89170e2d15959
MichalJanecka <167630007+MichalJanecka@users.noreply.github.com>
2024-08-16 10:42:12 +0200

    Paragraph indent

--- t_michal_janecka_project_SQL_secondary_final.sql
+++ t_michal_janecka_project_SQL_secondary_final.sql
@@ -1,8 +1,8 @@
 -- Table 2: creation
 CREATE OR REPLACE TABLE t_michal_janecka_project_SQL_secondary_final AS
-	SELECT *
-	FROM v_michal_yoy_price_growth vmypg 
-	JOIN v_michal_yoy_wage_growth vmywg 
+SELECT *
+FROM v_michal_yoy_price_growth vmypg 
+JOIN v_michal_yoy_wage_growth vmywg 
 	ON vmypg.selected_year = vmywg.payroll_year
-	JOIN economies
+JOIN economies
 	ON vmywg.payroll_year = economies.`year`;
